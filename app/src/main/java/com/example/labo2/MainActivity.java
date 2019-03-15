240862212cfa3e1e3862332bdeecf22f7e13b1e1
uca <00400815@uca.edu.sv>
2019-03-15 17:19:01 -0600

    act1

---
+++ app/src/main/java/com/example/labo2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.labo2;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button button;
+    private EditText edit;
+    private EditText edit2;
+    private TextView text;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        
+        button = findViewById(R.id.btn);
+        edit = findViewById(R.id.user);
+        edit2 = findViewById(R.id.pass);
+        text = findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(edit.getText());
+            }
+        });
+    }
+
+}
