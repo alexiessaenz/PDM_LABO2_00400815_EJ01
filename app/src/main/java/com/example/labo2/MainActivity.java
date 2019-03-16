1c868beca028a66861eaceaafac130a666973429
WEGNDES <00400815@uca.edu.sv>
2019-03-16 10:25:54 -0600

    act4

--- app/src/main/java/com/example/labo2/MainActivity.java
+++ app/src/main/java/com/example/labo2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.labo2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,9 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button button;
+public class MainActivity extends AppCompatActivity {
+    private Button boton;
     private EditText edit;
     private EditText edit2;
     private TextView text;
@@ -22,12 +25,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btn);
+        boton = findViewById(R.id.btn);
         edit = findViewById(R.id.user);
         edit2 = findViewById(R.id.pass);
         text = findViewById(R.id.text);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+       /* button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(edit.getText());
@@ -38,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 text.setText(edit2.getText());
                 return false;
             }
-        });
+        });*/
     }
 
 }
